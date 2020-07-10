4bc234494547d860a17d7031c86dedeeeed9afc6
squgeim <shreyadahal@gmail.com>
2020-07-09 23:11:09 -0400

    Fix title description and favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import SEO from "../components/seo"
-import { Link } from "gatsby";
 
 import 'reset-css/reset.css';
 import '../styles/style.css';
@@ -12,7 +11,7 @@ import FooterSection from "../components/footerSection"
 const IndexPage = () => {
   return (
     <>
-      <SEO description={"Shreya Dahal's personal blog and portfolio."} lang="en-US" title={"Shreya Dahal's Blog"} />
+      <SEO lang="en-US" title={"Blog"} />
       <LandingSection />
       <BlogSection />
       <FooterSection />
